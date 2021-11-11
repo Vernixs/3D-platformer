61455f695bdbe54a203427a57afa32c0353dafb1
Vernixs <enecelis2021@elam.co.uk>
2021-11-11 00:13:34 +0000

    Fix Movement
    Still need to find out why it does not move

--- Assets/Charactercontroller.cs
+++ Assets/Charactercontroller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Charactercontroller : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
 
     //Movement code and jumping
